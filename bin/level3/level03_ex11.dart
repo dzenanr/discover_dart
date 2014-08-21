262ac3145ead59f24c2a40a124a3a09030173b22
Dzenan Ridjanovic <dzenanr@gmail.com>
2014-08-21 17:33:58 -0400

    change a map value from string to list

--- bin/level3/level03_ex11.dart
+++ bin/level3/level03_ex11.dart
@@ -1,5 +1,5 @@
 main() {
-  var powers = {'The Dart':['Speed', 'Agility'], 'Prof. Polymer':'Super Hearing'};
+  var powers = {'The Dart':['Speed', 'Agility'], 'Prof. Polymer':['Super Hearing']};
   // ________; <- powers['Bullseye'] = ['Talking to animals']
   powers['Bullseye'] = ['Talking to animals'];
   print("Bullseye's status is: ${powers['Bullseye']}");
