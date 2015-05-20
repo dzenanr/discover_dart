8b4eb97ca3a88bf665d535e8d932aa3ce514c93d
Dzenan Ridjanovic <dzenanr@gmail.com>
2015-05-20 17:08:26 +0200

    comment print to avoid red color error

--- bin/level2/level02_ex10.dart
+++ bin/level2/level02_ex10.dart
@@ -1,5 +1,6 @@
 
 main() {
-  print(heroName);            // using the heroName variable
+  // uncomment print to use the heroName variable before it is declared
+  //print(heroName); 
   var heroName =  'The Dart'; // declaring the heroName variable
 }
